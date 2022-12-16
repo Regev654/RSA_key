11d7dabbb0b9e1b185e1c01f5efd1933661d94e8
ItamarParienty <parienty.itamar@gmail.com>
2022-12-16 11:39:14 +0200

    adding rsa functionality

--- number_theory_functions.py
+++ number_theory_functions.py
@@ -1,6 +1,29 @@
-from random import randrange
+from random import randrange, randint
 
-def extended_gcd(a,b):
+
+def generate_random_coprime(phi_n, max_digits = 10):
+    """
+
+    Parameters
+    ----------
+    max_digits - the generated number max length
+    number - the number we want the gen
+
+    Returns
+    -------
+    generate random number that (generated, number) = 1
+    """
+    found = False
+    number = 0
+    while not found:
+        i = randint(1, max_digits)
+        number = generate_prime(i)
+        found = extended_gcd(number, phi_n)[0] == 1
+
+    return number
+
+
+def extended_gcd(a, b):
     """
     Returns the extended gcd of a and b
 
@@ -13,27 +36,25 @@ def extended_gcd(a,b):
     (d, x, y): d = gcd(a,b) = a*x + b*y
     """
     if a == 0 and b == 0:
-        return 
+        return
     if a > b:
         temp = a
         a = b
         b = temp
-    
-    if a == 0 :
+
+    if a == 0:
         return (b, 0, 1)
-    
+
     r = b % a
     (gcd, newA, newB) = extended_gcd(a, r)
-         
-        
+
     x = newB - (b // a) * newA
     y = newA
-         
-    return (gcd, x, y)
-    
-   
 
-def modular_inverse(a,n):
+    return (gcd, x, y)
+
+
+def modular_inverse(a, n):
     """
     Returns the inverse of a modulo n if one exists
 
@@ -49,7 +70,7 @@ def modular_inverse(a,n):
     gcd_res = extended_gcd(a, n)
     if gcd_res[0] == 1:
         x = gcd_res[1]
-        return n+x if x <= 0 else x
+        return n + x if x <= 0 else x
     return None
 
 
@@ -69,16 +90,18 @@ def modular_exponent(a, d, n):
     """
     power = 0
     temp = 0
-    a_new = a%n
+    a_new = a % n
     mod_sum = 1
     binary_d = bin(d)
     print(binary_d)
     for i in binary_d[-1:1:-1]:
         number = int(i)
-        mod_sum*=a_new**(number*2**power)%n
-        mod_sum = mod_sum%n
+        mod_sum *= a_new ** (number * 2 ** power) % n
+        mod_sum = mod_sum % n
         power += 1
     return mod_sum
+
+
 def miller_rabin(n):
     """
     Checks the primality of n using the Miller-Rabin test
@@ -92,22 +115,23 @@ def miller_rabin(n):
     b: If n is prime, b is guaranteed to be True.
     If n is not a prime, b has a 3/4 chance at least to be False
     """
-    a = randrange(1,n)
+    a = randrange(1, n)
     k = 0
-    d = n-1
+    d = n - 1
     while d % 2 == 0:
         k = k + 1
         d = d // 2
     x = modular_exponent(a, d, n)
-    if x == 1 or x == n-1:
+    if x == 1 or x == n - 1:
         return True
     for _ in range(k):
         x = (x * x) % n
         if x == 1:
             return False
-        if x == n-1:
+        if x == n - 1:
             return True
     return False
+
 
 def is_prime(n):
     """
@@ -127,9 +151,10 @@ def is_prime(n):
             return False
     return True
 
+
 def generate_prime(digits):
     for i in range(digits * 10):
-        n = randrange(10**(digits-1), 10**digits)
+        n = randrange(10 ** (digits - 1), 10 ** digits)
         if is_prime(n):
             return n
     return None
--- rsa_functions.py
+++ rsa_functions.py
@@ -1,12 +1,19 @@
-import number_theory_functions
+from _ast import Expr
+
+from number_theory_functions import generate_prime, generate_random_coprime, modular_inverse, modular_exponent
+from random import random
+
+N_INDEX = 0
+E_INDEX = 1
+D_INDEX = 1
 
 class RSA():
-    def __init__(self, public_key, private_key = None):
+    def __init__(self, public_key, private_key=None):
         self.public_key = public_key
         self.private_key = private_key
 
     @staticmethod
-    def generate(digits = 10):
+    def generate(digits=10):
         """
         Creates an RSA encryption system object
 
@@ -20,7 +27,14 @@ class RSA():
         * The public key (N,e)
         * The private key (N,d)
         """
+        q = generate_prime(digits)
+        p = generate_prime(digits)
+        n = q * p
+        phi_n = (q - 1) * (p - 1)
 
+        e = generate_random_coprime(phi_n, digits)
+        d = modular_inverse(e, phi_n)
+        return RSA((n, e), (n, d))
 
     def encrypt(self, m):
         """
@@ -34,7 +48,7 @@ class RSA():
         -------
         c : The encrypted ciphertext
         """
-
+        return modular_exponent(m, self.public_key[E_INDEX], self.public_key[N_INDEX])
 
     def decrypt(self, c):
         """
@@ -48,3 +62,5 @@ class RSA():
         -------
         m : The decrypted plaintext
        """
+        return modular_exponent(c, self.private_key[D_INDEX], self.private_key[N_INDEX])
+
